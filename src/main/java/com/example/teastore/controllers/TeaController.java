85d3ab08c020c2a8652cf6afef06c2e139d5cf6f
Choorilove <spoun.1510@gmail.com>
2024-08-22 23:33:47 +0300

    controller edited

--- src/main/java/com/example/teastore/controllers/TeaController.java
+++ src/main/java/com/example/teastore/controllers/TeaController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -25,4 +27,19 @@ public class TeaController {
         model.addAttribute("itemsByCategory", itemsByCategory);
         return "tea";
     }
+
+
+
+        @GetMapping("/tea/{key}")
+        public String teaKind(@PathVariable("key") String key, Model model) {
+            List<Item> items = itemRepository.findAllSorted();
+            Map<String, List<Item>> itemsByCategory = items.stream()
+                    .collect(Collectors.groupingBy(Item::getSort));
+            model.addAttribute("itemsByCategory", itemsByCategory);
+            model.addAttribute("selectedKey", key);
+            return "kind";
+        }
+
+
+
 }
