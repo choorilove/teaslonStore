f002c869055767c02b60e4412ea3c05d25f064be
Choorilove <spoun.1510@gmail.com>
2024-08-25 02:34:32 +0300

    changed

--- src/main/java/com/example/teastore/models/Item.java
+++ src/main/java/com/example/teastore/models/Item.java
@@ -11,18 +11,75 @@ public class Item {
 
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String title, description, image1, image2, image3, sort;
+    private String title, description, image1, image2, image3, sort, weight,manufacturer,year,province,fabric,volume;
     private float price;
 
     public Item() {}
-    public Item(String title, String description, String image1, String image2, String image3, String sort, float price) {
+
+    public Item(String title, String description, String image1, String image2, String image3,
+                String sort, String weight, String manufacturer,
+                String year, String province, String fabric, String volume, float price) {
         this.title = title;
         this.description = description;
         this.image1 = image1;
         this.image2 = image2;
         this.image3 = image3;
         this.sort = sort;
+        this.weight = weight;
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.province = province;
+        this.fabric = fabric;
+        this.volume = volume;
         this.price = price;
+    }
+
+    public String getFabric() {
+        return fabric;
+    }
+
+    public void setFabric(String fabric) {
+        this.fabric = fabric;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
     }
 
     public long getId() {
